5f379df9dfc2388c74f49e5966081b52224e73b3
lehoangan1503 <49606872+lehoangan1503@users.noreply.github.com>
2022-06-12 10:27:13 +0700

    change href of a tag go to twitter project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ const projects = [
     projectUrlImage: twitterPostImage,
     projectName: "Twitter Post clone older version",
     sourceLink: "https://github.com/lehoangan1503/twitter-post-clone",
-    liveDemoLink: "https://anlehoang-portfolio.herokuapp.com/",
+    liveDemoLink: "https://anlehoang-twitter.herokuapp.com/",
   },
 ];
 const listProject = projects.map((project) => {
